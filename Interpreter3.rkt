3797dc4077bda970672822f74d2b127f895f0713
Sam Ehrenstein <sam.ehrenstein@gmail.com>
2020-04-27 07:25:27 -0400

    implement part 3 the right way in Interpreter3.rkt

--- Interpreter3.rkt
+++ Interpreter3.rkt
@@ -1,31 +1,12 @@
 ; Sam Ehrenstein
 ; Samantha Frankum
 ; Niharika Karnik
-
-; TODO 4/22: Make class definitions recursive
-
 #lang racket
-(require "classParser.rkt")
-
+(require "functionParser.rkt")
 ; interpret starts the parsing of the 
-
-(define create-classes
-  (lambda (filename)
-    (Mstate (parser filename) initialState (lambda (r) r) (lambda (k) (error 'flow "Breaking outside of while loop")) (lambda (c) c) initialError)))
-
-(define exec-function-closure
-  (lambda (closure name params state return throw)
-    (letrec ((body (cadr closure))
-          (formal-params (car closure))
-          (environment (Add name (list formal-params body state) (caddr closure))))
-          (if (not (equal? (length params) (length formal-params)))
-              (throw "function: incorrect number of parameters")
-              (return (Mstate body state return (lambda (k) (error 'flow "Breaking outside of while loop")) (lambda (c) c) throw))))))
-
 (define interpret
-  (lambda (filename classname)
-    (let ((toplevel-state (create-classes filename)))
-      (exec-function-closure (Lookup 'main (Lookup classname toplevel-state initialError) initialError) 'exec '() toplevel-state (lambda (r) r) initialError))))
+  (lambda (filename)
+    (Operate '(funcall main) (Mstate (parser filename) initialState (lambda (r) r) (lambda (k) (error 'flow "Breaking outside of while loop")) (lambda (c) c) initialError) initialError)))
 
 ;inital error is the default value of the error continuation
 (define initialError
@@ -33,10 +14,8 @@
 
 ; An Empty layer
 (define initialLayer '(()()))
-
 ; The initial value of the state
 (define initialState (list initialLayer))
-
 ; M_value (<value1> <value2> +, state) = M_value(<value1>, state) + M_value(<value2>, state)
 ; The following mathematical operations are implemented : +, -, *, /, % (including the unary -),
 ; the following comparison operators are implemented: ==, !=, <, >, <=. >=, and the following boolean operators: &&, ||, !.
@@ -53,8 +32,6 @@
       [(and (symbol? expression) (instantiated? expression state)) (Lookup expression state throw)]
       [(and (symbol? expression) (declared? expression state)) (throw "variable: Using variable without instantiating it first")]
       [(eq? (operator expression) 'funcall) (call/cc (lambda (r)(exec-function (cadr expression) (cddr expression) state r throw)))]
-      [(eq? 'dot (operator expression)) (Lookup (caddr expression) (Lookup (cadr expression) state throw) throw)]
-      [(eq? 'new (car expression)) (instance-closure (cadr expression) state throw)]
       [(symbol? expression) (throw "variable: Using variable without declaring it first")]
       [(and (eq? '- (operator expression)) (not (third? expression)) (- (Operate (leftoperand expression) state throw)))]
       [(eq? '+ (operator expression)) (+ (Operate (leftoperand expression) state throw) (Operate (rightoperand expression) state throw))]
@@ -73,12 +50,10 @@
       [(eq? '! (operator expression)) (booltoSymbol(not (symboltoBool(Operate (leftoperand expression) state throw) throw)) throw)]
       [else (throw "badop: The operator is not known")]
       )))
-
 ; Tests if the list has a third element.
 (define third?
   (lambda (lis)
     (pair? (cddr lis))))   
-
 ; General description of Mstate:
 ; When we declare a variable, it gets added to the end of the list of variables, with no corresponding value.
 ; This means that if we try to call Mvalue on it, it will raise an error, as Lookup will have to exhaust the list of values first.
@@ -97,47 +72,38 @@
       [(eq? '= (operator expression)) (throw "variable: Using variable out of scope")]
       [(eq? 'var (operator expression)) (Add* (leftoperand expression) state)]
       [(eq? 'begin (operator expression)) (RemoveLayer (Mstate (cdr expression) (AddLayer state) return break continue throw))]
-      [else (keyword expression state return break continue throw)])))
-
+      [(keyword? (operator expression)) (keyword expression state return break continue throw)])))
 ; Abstraction is maintained through the use of Add, Add*, Remove, and Lookup functions as the only ways of accessing the state.
 ; Add is exactly the way we defined in class: Add(name, value, state) -> state with value as the value of name.
 ; Only used with the = operator (and syntactic sugar like ++, maybe).
 (define Add
   (lambda (name value state)
     (cons (list (cons name (car (toplayer state))) (cons (box value) (cadr (toplayer state)))) (cdr state))))
-
 ; Gets the top layer
 (define toplayer car)
-
 ; Adds an emptylayer to the state
 (define AddLayer
   (lambda (state)
     (cons initialLayer state)))
-
 ; Removes top layer
 (define RemoveLayer cdr)
-
 ; Checks if the given list is a layered state
 (define layered?
   (lambda (lis)
     (and (pair? (car lis)) (list? (car (car lis))))))
-
 ; Add* is like Add, except it only adds a name. This corresponds to the var keyword. Since adding only happens through the state, we can control which one we use.
 (define Add*
   (lambda (name state)
     (cons (doublecons name (box '()) (toplayer state)) (cdr state))))
-
 ; Helper function. Takes two values and a list containing two lists, and returns the list with the values added to each list with cons.
 ; Also allows Remove to be O(n) instead of O(2^n) without using let.
 (define doublecons
   (lambda (v1 v2 lis)
     (cons (cons v1 (car lis)) (cons (cons v2 (cadr lis)) '()))))
-
 ; Takes a list containing two lists and returns a list containing the cdr of each list.
 (define doublecdr
   (lambda (lis)
     (cons (cdr (car lis)) (cons (cdr (cadr lis)) '()))))
-
 ; Check if a variable has been declared.
 ; Note that the list of values gets thrown out after the first call, as it is not relevant. This avoids the need to handle the case where it is null.
 ; This also uses recursion. If state has at least one layer (as opposed to being a single layer), we check each layer.
@@ -150,7 +116,6 @@
       [(layered? state) (or (declared? name (toplayer state)) (declared? name (cdr state)))]
       [(eq? name (car (car state))) #t]
       [else (declared? name (cons (cdr (car state)) '()))])))
-
 ; Sets the value of the variable in the given layer 
 (define set-value-in-layer
   (lambda (name value layer throw)
@@ -158,7 +123,6 @@
       [(null? (car layer)) (throw "variable: Variable not declared")]
       [(eq? (car (car layer)) name) (begin (set-box! (car (cadr layer)) value) layer)]
       [else (doublecons (car (car layer)) (car (cadr layer)) (set-value-in-layer name value (doublecdr layer) throw))])))
-
 ; Sets the value of the innermost variable with the given name
 (define SetValue
   (lambda (name value state throw)
@@ -166,7 +130,6 @@
       [(null? state) (throw "variable: Undeclared variable")]
       [(declared? name (toplayer state)) (cons (set-value-in-layer name value (toplayer state) throw) (cdr state))]
       [else (cons (toplayer state) (SetValue name value (cdr state) throw))])))
-
 ; Checks if a variable has been instantiated
 (define instantiated?
   (lambda (varname state)
@@ -177,7 +140,6 @@
       [(and (eq? varname (car (car state))) (not (null? (unbox (car (cadr state)))))) #t]
       [(eq? varname (car (car state))) #f]
       [else (instantiated? varname (cons (cdr (car state)) (cons (cdr (cadr state)) '())))])))
-
 ; Returns the value of a variable if it is instantiated, or throws an error if it isn't.
 (define Lookup
   (lambda (varname state throw)
@@ -187,23 +149,28 @@
       [(null? (cadr state)) (throw "state: variable has not been initialized")]  ; if instantiated is used in Mstate, should never happen
       [(eq? varname (car (car state))) (unbox (car (cadr state)))]
       [else (Lookup varname (cons (cdr (car state)) (cons (cdr (cadr state)) '())) throw)])))
-
-; Lookup, but without unboxing. Used to look up closures, which are not in boxes.
-(define Lookup-closure
-  (lambda (varname closure throw)
+; keyword? checks if value is a known keyword
+(define keyword?
+  (lambda (word)
     (cond
-      [(and (layered? closure) (declared? varname (toplayer closure))) (Lookup varname (toplayer closure) throw)]
-      [(layered? closure) (Lookup varname (cdr closure) throw)]
-      [(null? (cadr closure)) (throw "state: variable has not been initialized")]  ; if instantiated is used in Mstate, should never happen
-      [(eq? varname (car (car closure))) (car (cadr closure))]
-      [else (Lookup varname (cons (cdr (car closure)) (cons (cdr (cadr closure)) '())) throw)])))
-
+      [(null? word) #f]
+      [(eq? 'while word) #t]
+      [(eq? 'if word) #t]
+      [(eq? 'return word) #t]
+      [(eq? 'break word) #t]
+      [(eq? 'continue word) #t]
+      [(eq? 'try word) #t]
+      [(eq? 'catch word) #t]
+      [(eq? 'finally word) #t]
+      [(eq? 'throw word) #t]
+      [(eq? 'function word) #t]
+      [(eq? 'funcall word) #t]
+      [else #f])))
 ; keyword 
 (define keyword
   (lambda (expression state return break continue throw)
     (cond
-      [(null? (car expression)) (throw "state: keyword invalid")]
-      [(eq? 'class (car expression)) (Add (cadr expression) (class-closure (caddr expression) (cdddr expression) state return throw) state)]
+      [(null? (car expression)) (throw "state: keyword invalid")] ; this should never happen since check if keyword prior to calling keyword
       [(eq? 'while (car expression)) (call/cc (lambda (b)(while (cadr expression) (caddr expression) state return b continue throw)))]
       [(eq? 'if (car expression)) (if* (cadr expression) (caddr expression) (cdddr expression) state return break continue throw)]
       [(eq? 'break (car expression)) (break (RemoveLayer state))]
@@ -214,19 +181,23 @@
       [(eq? 'throw (car expression)) (throw (Operate (cadr expression) state throw))]
       [(eq? 'return (car expression)) (return (Operate (cadr expression) state throw))]
       [(eq? 'funcall (car expression)) (exec-function (cadr expression) (cddr expression) state (lambda (v) state) throw)]
-      [(or (eq? 'function (car expression)) (eq? 'static-function (car expression))) (Add (cadr expression) (list (caddr expression) (cadddr expression) state) state)])))
-  
-; Creates the class closure
-(define class-closure
-  (lambda (parent body state return throw)
-    (if (null? parent)
-        (Mstate body initialState return (lambda (b) (error 'flow "Breaking outside of loop")) (lambda (c) c) initialError)
-        (Mstate body (Add 'super (Lookup (cadr parent) state)) initialState return (lambda (b) (error 'flow "Breaking outside of loop")) (lambda (c) c) initialError))))
+      [(eq? 'function (car expression)) (Add (cadr expression) (list (caddr expression) (cadddr expression)                                                                   (lambda (s t) (make-env (cadr expression) s t))) state)])))
 
-; Creates the instance closure, which is just a copy of the class closure
-(define instance-closure
-  (lambda (class state throw)
-    (Lookup class state throw)))
+(define pop-toplayer cdr)
+
+; Create the state for executing a function from the current state when the function is called.
+; We do a cascading recursive list search for the function, first looking for the layer it is in, and then its actual defintion.
+; When we hit the function name, the current state contains the function closure and everything that was defined before it.
+; This is exactly the same as its scope, so we just return the state.
+(define make-env
+  (lambda (name state throw)
+    (cond
+      [(null? state)(throw "state: state should not be empty")]
+      [(and (layered? state) (declared? name (toplayer state))) (cons (make-env name (toplayer state) throw) (pop-toplayer state))]
+      [(layered? state) (make-env name (pop-toplayer state) throw)]
+      [(null? (cadr state)) (throw "state: function not in scope")]
+      [(eq? name (car (car state))) state]
+      [else (make-env name (doublecdr state) throw)])))
 
 ;exec-function will execute a function
 (define exec-function
@@ -234,18 +205,18 @@
     (letrec ((closure (Lookup name state throw))
           (body (cadr closure))
           (formal-params (car closure))
-          (environment (Add name (list formal-params body state) (caddr closure))))
+          (env-func (caddr closure))
+          (environment (env-func state throw)))
           (if (not (equal? (length params) (length formal-params)))
               (throw "function: incorrect number of parameters")
               (return (Mstate body (make-refs environment state formal-params params throw) return (lambda (k) (error 'flow "Breaking outside of while loop")) (lambda (c) c) throw))))))
 
-;make-refs will bind formal parameter names to actual parameters
+;make-refs will bind formal parameter names to actual parameter 
 (define make-refs
   (lambda (env state fp ap throw) 
     (if (null? fp)
        env
        (make-refs (Add (car fp) (Operate (car ap) state throw) env) state (cdr fp) (cdr ap) throw))))
-
 ; try* defines how to handle the keyword 'try
 (define try*
   (lambda (try catch finally state return break continue throw)
@@ -253,19 +224,16 @@
     (if (list? temp)
         (Mstate finally temp return break continue throw)
         (Mstate finally (Mstate (caddr catch) (Add (car (cadr catch)) temp state) return break continue throw) return break continue throw))))
-
 ; while* defines how to handle the keyword 'while
 (define while*
   (lambda (condition body state return break continue throw)
     (if (symboltoBool (Operate condition state throw) throw)
         (while* condition body (Mstate body state return break continue throw) return break continue throw)
         (break state))))
-
 ; while defines how to handle the keyword 'while
 (define while
   (lambda (condition body state return break continue throw)
     (while condition body (RemoveLayer (call/cc (lambda (c) (while* condition body state return break c throw)))) return break continue throw)))
-
 ; if* defines how to handle the keyword 'if'
 (define if*
   (lambda (condition body otherwise state return break continue throw)
@@ -274,12 +242,10 @@
         (if (null? otherwise)
             state
             (Mstate (car otherwise) state return break continue throw)))))
-
 ; Assumes infix notation
 (define operator car)
 (define leftoperand cadr)
 (define rightoperand caddr)
-
 ; Convert boolean to symbol
 (define booltoSymbol
   (lambda (bool throw)
@@ -297,7 +263,6 @@
       [(eq? val 'true) #t]
       [(eq? val 'false) #f]
       [else (throw " bool: Non-boolean symbol provided")])))
-
 ; Convert value to String
 (define valueToString
   (lambda (val)
